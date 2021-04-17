19ae9dc210ff029b1a6ef6ee0b2d81dd0a4b7793
russkev <russkev@gmail.com>
2021-04-18 00:09:58 +1000

    Avatar going

--- my-app/src/shared/components/avatar.tsx
+++ my-app/src/shared/components/avatar.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import Box from '@material-ui/core/Box';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Box from "@material-ui/core/Box";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => createStyles({
-  profile: {
-    backgroundImage: 'url(https://images.unsplash.com/photo-1542297919088-a2b78b2c578b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80)',
-  },
-}));
+const useStyles = makeStyles(() => {
+  return createStyles({
+    profile: {
+      backgroundImage: `url(https://images.unsplash.com/photo-1542297919088-a2b78b2c578b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80)`,
+      backgroundPosition: "50%",
+      borderRadius: "50%",
+    },
+  });
+});
 
 const Avatar = () => {
   const classes = useStyles();
--- my-app/src/shared/components/class.list.tsx
+++ my-app/src/shared/components/class.list.tsx
@@ -1,32 +1,57 @@
-import React from 'react';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import Avatar from './avatar';
+import React from "react";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+// import Avatar from "./avatar";
 
-const ClassList = () => (
-  <>
-    <Avatar />
-    <Typography variant="h3">
-      Jasmine
-    </Typography>
-    <Typography>
-      Year 7, Morley High School
-    </Typography>
-    <Divider />
-    <List>
-      <ListItem button>
-        <ListItemText primary="All Classes" />
-      </ListItem>
-      {['Maths', 'Science', 'English', 'History', 'Chinese'].map((text) => (
-        <ListItem button key={text}>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
-  </>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+    small: {
+      width: theme.spacing(3),
+      height: theme.spacing(3),
+    },
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+  })
 );
+
+const ClassList = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Avatar
+        alt="Jasmine"
+        src="https://images.unsplash.com/photo-1542297919088-a2b78b2c578b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+        className={classes.large}
+      />
+      <Typography variant="h3">Jasmine</Typography>
+      <Typography>Year 7, Morley High School</Typography>
+      <Divider />
+      <List>
+        <ListItem button>
+          <ListItemText primary="All Classes" />
+        </ListItem>
+        {["Maths", "Science", "English", "History", "Chinese"].map((text) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+};
 
 export default ClassList;
