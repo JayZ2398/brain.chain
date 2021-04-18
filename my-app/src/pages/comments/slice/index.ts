6e41f93ae88ab8980591dcedd65d0a0e1667c31c
Jonah Sol <jonahsol@tutanota.com>
2021-04-18 13:09:33 +1000

    Add comments/slice

---
+++ my-app/src/pages/comments/slice/index.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../../shared/redux/rootReducer";
+
+import { comments as initComments } from "../../../shared/data";
+import { Comment } from "../../../shared/models";
+
+export interface CommentsState {
+  comments: Comment[];
+}
+
+// Slice
+
+const commentsInitState: CommentsState = {
+  comments: initComments,
+};
+
+const comments = createSlice({
+  name: "comments",
+  initialState: commentsInitState,
+  reducers: {
+    setComments: (state, { payload }: PayloadAction<Comment[]>) => {
+      state.comments = payload;
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { actions } = comments;
+
+export default comments.reducer;
+
+export const selectComments = (state: RootState) => state.comments;
--- my-app/src/shared/redux/rootReducer.ts
+++ my-app/src/shared/redux/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import squadReducer from "../../pages/squads/slice";
+import commentReducer from "../../pages/comments/slice";
 
 const rootReducer = combineReducers({
   squads: squadReducer,
+  comments: commentReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
